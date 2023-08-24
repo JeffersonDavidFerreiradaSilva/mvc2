3f0554c115019990094d242bacb204379b4d4113
Jefferson David <napsterdavid@gmail.com>
2023-08-24 19:12:33 -0300

    view que obtem os dados do aluno

---
+++ view/pagina_cadastro_aluno.py
@@ -0,0 +1,15 @@
+class cadastro:
+
+
+    @staticmethod
+
+    def pegar_dados_aluno():
+        nome = input("DIGITE O NOME DO ALUNO: ")
+        idade = int(input("DIGITE A IDADE DO ALUNO: "))
+        peso = float(input("DIGITE O PESO DO ALUNO: "))
+        return nome, idade, peso
+    
+    @staticmethod
+
+    def mostrarmensagem (menssagem):       
+        print (menssagem)
